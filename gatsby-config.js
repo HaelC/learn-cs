97810663ca990cda5d64f16d5ab3d98b37844321
Hael Chan <f.procumbens@gmail.com>
2020-06-14 22:10:11 -0700

    icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,13 +19,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Learn Computer Science`,
+        short_name: `LearnCS`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/learn-cs-icon.png`, // This path is relative to the root of the site.
       },
     },
     {
