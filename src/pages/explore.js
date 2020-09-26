286d485fd4d4390afd6e68c004d36b8c5e06b6d3
HaelChen <f.procumbens@gmail.com>
2020-09-25 19:57:55 -0700

    add icons to tags

--- src/pages/explore.js
+++ src/pages/explore.js
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 import { graphql, Link } from "gatsby";
 import Layout from "../components/layout";
 import kebabCase from "lodash/kebabCase";
+import Img from "gatsby-image";
+import { css } from "@emotion/core";
 import SEO from "../components/seo";
 
 export const query = graphql`
@@ -13,26 +15,60 @@ export const query = graphql`
         totalCount
       }
     }
+    icons: allFile(filter: { relativeDirectory: { eq: "icons" } }) {
+      nodes {
+        childImageSharp {
+          fixed(width: 32, height: 32) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+        name
+      }
+    }
   }
 `;
 
-const Explore = ({
-  data: {
-    tags: { group },
-  },
-}) => {
+const Explore = ({ data }) => {
+  const icons = {};
+  data.icons.nodes.forEach(icon => {
+    icons[icon.name] = icon.childImageSharp;
+  });
+  const group = data.tags.group;
   group.sort((a, b) => b.totalCount - a.totalCount);
   return (
     <Layout>
       <SEO title="Explore" description="Explore content within Learn CS" />
       <h2>TAGS</h2>
-      <ul>
+      <ul
+        css={css`
+          list-style-type: none;
+        `}
+      >
         {group.map(tag => (
           <li key={tag.fieldValue}>
             <Link to={`/tag/${kebabCase(tag.fieldValue)}`}>
-              {tag.fieldValue === "Cpp" ? "C++" : tag.fieldValue}
+              <div
+                css={css`
+                  height: 32px;
+                  display: flex;
+                  align-items: center;
+                `}
+              >
+                {icons[tag.fieldValue] ? (
+                  <Img fixed={icons[tag.fieldValue].fixed} />
+                ) : (
+                  <Img fixed={icons.Default.fixed} />
+                )}
+                <span
+                  css={css`
+                    padding-left: 10px;
+                  `}
+                >
+                  {tag.fieldValue === "Cpp" ? "C++" : tag.fieldValue}
+                </span>
+              </div>
             </Link>
-            ({tag.totalCount})
+            {/* ({tag.totalCount}) */}
           </li>
         ))}
       </ul>
